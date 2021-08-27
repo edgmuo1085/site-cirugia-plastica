a83e1f92ad81a4664fa1795070620ed8e7b0b28a
Edgar Muñoz <edgmuo1085@gmail.com>
2021-08-27 00:15:10 -0500

    Se realiza ajustes de estilos y agregacion de funciones para SEO

---
+++ lib/funtions.php
@@ -0,0 +1,33 @@
+<?php
+date_default_timezone_set('America/Bogota');
+
+
+class Funcciones
+{
+    private $host;
+    private $uri;
+
+    public function getHost()
+    {
+        return $this->host;
+    }
+    public function getUri()
+    {
+        return $this->uri;
+    }
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+}
+
+$site = new Funcciones();
+$site->setHost($_SERVER["HTTP_HOST"]);
+$site->setUri($_SERVER["REQUEST_URI"]);
+$ruta = 'http://' . $site->getHost() . $site->getUri();
+
+$date = Date("Y-m-d H:i:s");
